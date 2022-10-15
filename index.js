8414751aaa468b26c323d9b9572d135d6dba63fd
Felipe Gallego <felipegallego600@gmail.com>
2022-10-15 18:52:24 -0500

    prueba

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const estudios1= document.querySelector("#estu1")
 const estudios2= document.querySelector("#estu2")
 const estudios3= document.querySelector("#estu3")
@@ -6,26 +7,55 @@ const estudios4= document.querySelector("#estu4")
 
 const estutext= document.querySelector("#estu_text")
 const estutitle= document.querySelector("#estu_title")
+co();
+def();
 
+estudios1.addEventListener("click",def, false);
 
-estudios1.addEventListener("click",(()=>{
+function def(){
     estutitle.innerHTML="Universitario"
-    estutext.innerHTML="asd"
-}), false);
+    estutext.innerHTML=`
+    <b>2024 > Politecnico Grancolombiano.</b> <br> 
+    Ingenieria en Sistemas. <br> <br>
+    <b>2021 - 2023 > Politecnico Grancolombiano.</b> <br> 
+    Tecnologia en Administracion de sistemas. <br> <br>
+    `
+    co()
+    estudios1.classList.add("foo")
+}
 
 estudios2.addEventListener("click",(()=>{
     estutitle.innerHTML="Diplimado"
-    estutext.innerHTML="asd"
+    estutext.innerHTML=`
+    <b>2021 > Mision Tic universidad de caldas</b> <br> 
+    Diplomado 800 horas en habilidades de programacion enfasis en programacion web.  <br> <br>
+    <b>2022 > Industria de talentos</b> <br> 
+    Diplomado en inteligencia artificial<br> <br>
+    `
+    co()
+    estudios2.classList.add("foo")
 }), false);
 
 estudios3.addEventListener("click",(()=>{
     estutitle.innerHTML="Colegio"
-    estutext.innerHTML="asd"
+    estutext.innerHTML=`
+    <b>2020 > Colegio Empresarial</b> <br> 
+    Mediatecnica en sistemas
+    `
+    co()
+    estudios3.classList.add("foo")
 }), false);
 
 estudios4.addEventListener("click",(()=>{
     estutitle.innerHTML="Logros"
-    estutext.innerHTML="asd"
+    estutext.innerHTML=`
+    <b>2020 > Fedesoft </b> <br> 
+    Finalista en el IV Concurso nacional de programación 2020 con el proyecto WorldLO <br> <br>
+    <b>2018 > Pygmalion</b> <br> 
+    Ganador concuro robotica Uvas<br> <br>
+    `
+    co()
+    estudios4.classList.add("foo")
 }), false);
 
 /*
@@ -36,11 +66,10 @@ estudios4.addEventListener("click",(()=>{
 */
 
 
-const sec1= document.querySelector("#Estudios")
-const sec2= document.querySelector("#estu2")
-const sec3= document.querySelector("#estu3")
-const sec4= document.querySelector("#estu4")
 
-sec2.addEventListener("scroll",(()=>{
-    console.log("me estas viendo")
-}))
+ function co(){
+    estudios1.classList.remove("foo");
+    estudios2.classList.remove("foo");
+    estudios3.classList.remove("foo");
+    estudios4.classList.remove("foo");
+ }
